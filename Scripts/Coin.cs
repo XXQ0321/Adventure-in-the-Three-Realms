9f59e32ee6a709cce71ded3608a261c50937786a
XXQ0321 <163402500+XXQ0321@users.noreply.github.com>
2025-04-21 04:33:40 +0800

    Update Coin

--- Scripts/Coin.cs
+++ Scripts/Coin.cs
@@ -5,30 +5,25 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    public float range; // 上下浮动范围
-    public float rotateSpeed; // 自转速度
+    public float range; 
+    public float rotateSpeed;
     
     private void Update()
     {
-        if (Time.timeScale == 0)//当游戏结束时 金币停止转动
+        if (Time.timeScale == 0)
         {
             return;
         }
-        
-        // 自转
-        transform.Rotate(0 ,0, rotateSpeed * Time.deltaTime);//金币自身围绕y轴旋转
-        // 浮动
-        transform.position += Mathf.Sin(Time.time) * Vector3.up * range * Time.deltaTime;//金币位置随机变化
+        transform.Rotate(0 ,0, rotateSpeed * Time.deltaTime);
+        transform.position += Mathf.Sin(Time.time) * Vector3.up * range * Time.deltaTime;
     }
 
-    private void OnTriggerEnter(Collider other)//碰撞检测
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag.Equals("Player"))//当金币触碰玩家时候
+        if (other.transform.tag.Equals("Player"))
         {
-            Destroy(gameObject);//销毁金币
-            
-            // 增加金币
-            FindObjectOfType<GameManager>().AddGoin();//ui数量增加1
+            Destroy(gameObject);
+            FindObjectOfType<GameManager>().AddGoin();
         }
     }
 }
